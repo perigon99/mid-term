84b2ada62c5b7db6338b16f312a612415b3bf53e
Christopher Kang <christopherjkang24@gmail.com>
2021-02-10 20:06:33 +0000

    eslint and clean code app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,49 +1,22 @@
-// const { response } = require("express");
-
-$(document).ready(function () {
-  //-----------------------------Navbar component------------------------------------------------------
-  const navbar = function () {
+$(document).ready(function() {
+  //-----------------------------Navbar Component / Login Rendering ------------------------------------------------------
+  const navbar = function() {
     const userObject = false; //Retrive and parse cookie for user info if no user logged in set to false
-    let $conditionalRendering = ""
+    let $conditionalRendering = "";
     if (!userObject) {
       $conditionalRendering = `
         <button class="modal-open bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-2 px-4 rounded-full" id="login-button">Log in</button>
         <button class="bg-transparent border border-gray-500 hover:border-indigo-500 text-gray-500 hover:text-indigo-500 font-bold py-2 px-4 rounded-full hidden" id="logout-button" >Log out</button>
-      `
-    }
-    else  {
+      `;
+    } else  {
       $conditionalRendering = `
         <li class="p-5">Welcome : ${userObject.name}</li>
         <li class="p-5"><a >Logout</a></li>
-      `
+      `;
     }
     $("#navbar").prepend($conditionalRendering);
-  }
-  //--------------------------------Login form rendering----------------------------------------------
-  const loginForm = function () {
-    let $section = `
-      <h3>Login to Restaurant App:</h3>
-      <form method="POST" action="/login">
-      <div class="userform-group">
-        <label for="userID">User ID:</label>
-        <input class="form-control" type="userID" name="userID" placeholder="Enter userID here" style="width: 1000px">
-      </div>
-      <div class="userform-group">
-        <label for="password">Password:</label>
-        <input class="form-control" type="password" name="password" placeholder="Enter password here" style="width: 1000px">
-      </div>
-      <button type="submit" class="btn btn-dark" style="margin-top: 1em;" > Login </button>
-      </form>
-    `
-    $(".bodyContent").prepend($section);
-  }
-  const registerForm = function () {
-    //Insert login form conditional rendering here
-    $(".bodyContent").prepend($section);
-  }
-
-  //--------------------------------Function calling ------------------------------------------------
-
+  };
+  //-------------------------------- Login Authentication: POST ------------------------------------------------
   $('#login-form').submit(function(event) {
     event.preventDefault();
     const formContent = $(this).serialize();
@@ -52,10 +25,9 @@ $(document).ready(function () {
       url: `/login`,
       method: 'POST',
       data: formContent,
-      success: function(result){
+      success: function(result) {
         console.log("everything went well. ", result);
-        if(result.name){
-          //do whatever you want
+        if (result.name) {
           alert("The user is authenticated");
           toggleModal();
           const loginButton = document.getElementById("login-button");
@@ -63,114 +35,102 @@ $(document).ready(function () {
 
           loginButton.style.display = "none";
           logoutButton.style.display = "block";
-          $("#navbar").prepend(`<div class="welcome-message">Welcome: ${result.name}</div>`)
-        } else{
+          $("#navbar").prepend(`<div class="welcome-message">Welcome: ${result.name}</div>`);
+        } else {
           //user is not authenticated
           alert("user / password is not correct");
         }
       },
-      error: function(error){
+      error: function(error) {
+        console.log(error);
       }
-    })
-      // .done(() => console.log('Its working!'))
-      // .fail(() => console.log('Error'))
-      // .always(() => console.log('Request Completed'))
+    });
   });
 
-//   $.get("http://localhost:8080/menu", function(data, status){
-//     console.log("everything went well. ", status, "My data is", data);
-//     console.log(data.length)
-//   }
-// )
-  const openUserLoginForm = function () {
-    var openmodal = document.querySelectorAll('.modal-open')
-    for (var i = 0; i < openmodal.length; i++) {
-      openmodal[i].addEventListener('click', function (event) {
-        event.preventDefault()
-        toggleModal()
-      })
+  //--------------------------------Modal Form Popup on Clicking Login ------------------------------------------------
+  const openUserLoginForm = function() {
+    let openmodal = document.querySelectorAll('.modal-open');
+    for (let i = 0; i < openmodal.length; i++) {
+      openmodal[i].addEventListener('click', function(event) {
+        event.preventDefault();
+        toggleModal();
+      });
     }
-    const overlay = document.querySelector('.modal-overlay')
-    overlay.addEventListener('click', toggleModal)
-    var closemodal = document.querySelectorAll('.modal-close')
-    for (var i = 0; i < closemodal.length; i++) {
-      closemodal[i].addEventListener('click', toggleModal)
+    const overlay = document.querySelector('.modal-overlay');
+    overlay.addEventListener('click', toggleModal);
+    let closemodal = document.querySelectorAll('.modal-close');
+    for (let i = 0; i < closemodal.length; i++) {
+      closemodal[i].addEventListener('click', toggleModal);
     }
-    document.onkeydown = function (evt) {
-      evt = evt || window.event
-      var isEscape = false
+    document.onkeydown = function(evt) {
+      evt = evt || window.event;
+      let isEscape = false;
       if ("key" in evt) {
-        isEscape = (evt.key === "Escape" || evt.key === "Esc")
+        isEscape = (evt.key === "Escape" || evt.key === "Esc");
       } else {
-        isEscape = (evt.keyCode === 27)
+        isEscape = (evt.keyCode === 27);
       }
       if (isEscape && document.body.classList.contains('modal-active')) {
-        toggleModal()
+        toggleModal();
       }
     };
     function toggleModal() {
-      const body = document.querySelector('body')
-      const modal = document.querySelector('.modal')
-      modal.classList.toggle('opacity-0')
-      modal.classList.toggle('pointer-events-none')
-      body.classList.toggle('modal-active')
+      const body = document.querySelector('body');
+      const modal = document.querySelector('.modal');
+      modal.classList.toggle('opacity-0');
+      modal.classList.toggle('pointer-events-none');
+      body.classList.toggle('modal-active');
     }
   };
-
-
-
-
-//--------------------------------Menu rendering---------------------------------------------------
-
-
-const entryHelper = function(rows) {
-  let menuEntries = "";
-  for (let row of rows) {
-    if(row.type_plate === "entry") {
-      menuEntries += `<li class="addCart" id="${row.id}">${row.name} ----- ${row.price} $</li>`
+  //--------------------------------Menu rendering---------------------------------------------------
+  const entryHelper = function(rows) {
+    let menuEntries = "";
+    for (let row of rows) {
+      if (row.type_plate === "entry") {
+        menuEntries += `<li class="addCart" id="${row.id}">${row.name} ----- ${row.price} $</li>`;
+      }
     }
-  }
-  return menuEntries;
-}
+    return menuEntries;
+  };
 
-const mainHelper = function(rows) {
-  let menuEntries = "";
-  for (let row of rows) {
-    if(row.type_plate === "main") {
-      menuEntries += `<li class="addCart" id="${row.id}">${row.name} ----- ${row.price} $</li>`
+  const mainHelper = function(rows) {
+    let menuEntries = "";
+    for (let row of rows) {
+      if (row.type_plate === "main") {
+        menuEntries += `<li class="addCart" id="${row.id}">${row.name} ----- ${row.price} $</li>`;
+      }
     }
-  }
-  return menuEntries;
-}
-const dessertHelper = function(rows) {
-  let menuEntries = "";
-  for (let row of rows) {
-    if(row.type_plate === "dessert") {
-      menuEntries += `<li class="addCart" id="${row.id}">${row.name} ----- ${row.price} $</li>`
+    return menuEntries;
+  };
+
+  const dessertHelper = function(rows) {
+    let menuEntries = "";
+    for (let row of rows) {
+      if (row.type_plate === "dessert") {
+        menuEntries += `<li class="addCart" id="${row.id}">${row.name} ----- ${row.price} $</li>`;
+      }
     }
-  }
-  return menuEntries;
-}
+    return menuEntries;
+  };
 
-const cellarHelper = function(rows) {
-  let menuEntries = "";
-  for (let row of rows) {
-    if(row.type_plate === "wine") {
-      menuEntries += `<li class="addCart" id="${row.id}">${row.name} ----- ${row.price} $</li>`
+  const cellarHelper = function(rows) {
+    let menuEntries = "";
+    for (let row of rows) {
+      if (row.type_plate === "wine") {
+        menuEntries += `<li class="addCart" id="${row.id}">${row.name} ----- ${row.price} $</li>`;
+      }
     }
-  }
-  return menuEntries;
-}
+    return menuEntries;
+  };
 
+  let menuItems;
 
-let menuItems
-
-const renderMenu = function () {
+  const renderMenu = function() {
   //Insert menu conditional rendering here
   //need ajax call to the backen to get menu information where is_active is true (Warning for now all menu element are false)
-  $.get("/menu", function(data, status){
-    menuItems = data.data.rows
-    let $body =`
+    $.get("/menu", function(data, status) {
+      menuItems = data.data.rows;
+      let $body = `
     <div class="flex pt-5 z-0">
       <div class="max-w-7xl mx-auto rounded overflow-hidden shadow-lg flex-1 border-double border-4 border-black menu-item">
           <div class="flex pt-2">
@@ -223,255 +183,100 @@ const renderMenu = function () {
           </div>
       </div>
     </div>
-    `
-    $(".bodyContent").prepend($body);
-  })
-}
-
-//-------------------------------- Check Cart Rendering ---------------------------------------------------
-
-let cart = [];
-
-const createOrder = function(cart) {
-  if (cart) {
-    console.log(cart);
-    $.ajax({
-      url: `/cart`,
-      method: 'POST',
-      contentType: "application/json",
-      data: JSON.stringify(cart),
-      success: function(result) {
-        console.log(result);
-      },
-      error: function(error) {
-        console.log(error);
-      }
+    `;
+      $(".bodyContent").prepend($body);
     });
+  };
+  //-------------------------------- Check Cart Rendering ---------------------------------------------------
 
-  }
+  // Stretch: object => key = id , value = quantity : displaying the cart => lookup the key in this object => food item
+  let cart = [];
 
-};
+  const createOrder = function(cart) {
+    if (cart) {
+      console.log(cart);
+      $.ajax({
+        url: `/cart`,
+        method: 'POST',
+        contentType: "application/json",
+        data: JSON.stringify(cart),
+        success: function(result) {
+          console.log(result);
+        },
+        error: function(error) {
+          console.log(error);
+        }
+      });
+    }
+  };
 
+  $(document).on("click", ".addCart", function(event) {
 
-
-// JSON.stringify(cart);
-
-// object => key = id , value = quantity
-// displaying the cart => lookup the key in this object => food item
-
-
-  $(document).on("click", ".addCart", function (event) {
-    // console.log(menuItems);
-
-    // if addcart => on click this specific class , do the below
     let foodItemId = parseInt(event.target.id);
-    // console.log("is it firing?", foodItemId);
-    let data = menuItems.filter(item => item.id === foodItemId)
 
-
-    //  console.log(data);
-    //  console.log(data[0].name)
+    let data = menuItems.filter(item => item.id === foodItemId);
     if (data) {
-      cart.push(data[0])
-      // cartInfo(cart);
-
+      cart.push(data[0]);
     }
-
+    // Remove this console log at end of project:
     console.log("Added to cart:" , cart);
-    // for (let item of cart) {
-
-    // console.log(`Added to cart: ${item.name} , $${item.price}`)
-
-    // if (cart.includes(item.name)) {
-    //   console.log("Is it working?");
-    // }
-    // }
-
-  })
-
-
-  // console.log(cart)
-
-  // const cartInfo = function (foodCart) {
-
-  //   for (let item of foodCart) {
-  //     // console.log("foodcart here", foodCart);
-  //     // console.log("items in the cart", item)
-  //   }
-  // }
-
-
-
-  // console.log("Added to cart:" , cart);
-
-  // $(".addCart").click(function() {
-  //   // console.log("thisOne", cart)
-  //   ("#testappend").append("<div>test</div>");
-  // })
-
-let foodItemCounter = 0;
-
-
-$(document).ready(function() {
-  $("#formButton").click(function() {
-    $("#form1").toggle();
-    console.log("items in cart:", cart);
-
-    let subtotalCounter = 0;
-    let quantityCounter = 0;
-
-    $("#food").empty();
-
-
-
-    for (let row in cart) {
-
-      console.log(cart[row]);
-      $("#food").append(`<div class="${foodItemCounter}">${cart[row].name} - $${cart[row].price} <button class="delete" data-index="${row}"> Delete </button> </div> `);
-
-      subtotalCounter += cart[row].price
-      quantityCounter += 1;
-      foodItemCounter += 1;
-
-    }
-
-    // cart.forEach(item => {
-    //   // console.log("items in the cart", item)
-    //   $("#food").append(`<div class="${foodItemCounter}">${item.name} - $${item.price} <button class="delete" id="${item.id}"> Delete </button> </div> `);
-
-    //   subtotalCounter += item.price;
-    //   quantityCounter += 1;
-    //   foodItemCounter += 1;
-    // })
-
-    $("#stotal").text(subtotalCounter);
-    $("#sub-total").text(subtotalCounter);
-    $("#quantity").text(`Total Quantity: ${quantityCounter}`);
-
-    // console.log(subtotalCounter);
-    // console.log(quantityCounter);
-
-  $("#Pay").click(function(event) {
-    event.preventDefault()
-    console.log("Is the cart in here?", cart);
-    createOrder(cart);
-  })
-      // console.log("test name finder", item.name)
-
-
-    // $("#testappend").append("<div>test</div>");
   });
 
-// ----------------------- Delete Rendering for Check Cart --------------------------
-  $("body").on("click", ".delete", function (event) {
 
-    event.preventDefault();
+  $(document).ready(function() {
+    $("#formButton").click(function() {
+      $("#form1").toggle();
+      // Remove this console log at end of project:
+      console.log("items in cart:", cart);
 
-    // if (cart) {
-    //   console.log(cart);
-    //   $.ajax({
-    //     url:`/cart`,
-    //     method: 'DELETE',
-    //     contentType: "application/json",
-    //     data: JSON.stringify(cart),
-    //     success: function(result) {
-    //       console.log(result);
-    //     },
-    //     error: function(error) {
-    //       console.log(error);
-    //     }
-    //   })
-    // }
+      let subtotalCounter = 0;
+      let quantityCounter = 0;
 
-    // let foodItemId = parseInt(event.target.id);
+      $("#food").empty();
 
-    // let data = cart.filter(item => item.id === foodItemId)
+      for (let row in cart) {
 
-    // if (data) {
-    //   const index = cart.indexOf(item)
-    //   cart.splice(index, 1)
-    // }
+        // console.log(cart[row]);
+        $("#food").append(`<div>${cart[row].name} - $${cart[row].price} <button class="delete" data-index="${row}"> Delete </button> </div> `);
 
-    // let apps = [{id:34,name:'My App',another:'thing'},{id:37,name:'My New App',another:'things'}];
+        subtotalCounter += cart[row].price;
+        quantityCounter += 1;
+      }
 
-    // get index of object with id:37
+      $("#stotal").text(subtotalCounter);
+      $("#sub-total").text(subtotalCounter);
+      $("#quantity").text(`Total Quantity: ${quantityCounter}`);
 
-    // ======================================
-    // for (let row in cart) {
+      $("#Pay").click(function(event) {
+        event.preventDefault();
+        console.log("Final Order sent to backend:", cart);
+        createOrder(cart);
+      });
+    });
 
-    //   console.log(cart[row]);
-    //   $("#food").append(`<div class="${foodItemCounter}">${cart[row].name} - $${cart[row].price} <button class="delete" id="${row}"> Delete </button> </div> `);
+    // ----------------------- Delete Rendering for Check Cart --------------------------
+    $("body").on("click", ".delete", function(event) {
 
+      event.preventDefault();
 
-    console.log($(event.target).data("index"));
+      cart.splice($(event.target).data("index"), 1);
 
-    // let removeIndex = cart.map(function(item) { return item.id; }).indexOf($(event.target).data("index"));
-    // console.log(`deleted at position ${removeIndex}, ${cart}`)
-    cart.splice($(event.target).data("index"), 1);
+      $(event.target).parent().remove();
+    });
 
+  });
 
-    console.log("Item Deleted");
-    // ======================================
-
-    $(event.target).parent().remove();
-    // const formContent = $(this).serialize();
-    // console.log(formContent);
-
-        // console.log("is it working?", result)
-        // $().remove();
-        // $(".delete").remove();
-
-  })
-
-
-  console.log("Food Counter:", foodItemCounter);
-});
-// ----------------------- Add Order Info to Database for User --------------------------
-
-
-// On click of menu item, link the orderID of the logged in user to the cart
-
-// On click of the menu item, add the menu item to the orders_content database;
-
-
- // On click => send a POST request to our psql database ..
- //
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ----------------------------------------------------------------------------------------
-
-navbar();
-openUserLoginForm();
-renderMenu();
-
-
-
-  $(document).ready( function () {
+  //--------------------------------Logout rendering----------------------------------------------
+  $(document).ready(function() {
     const logoutButton = document.getElementById("logout-button");
     $(logoutButton).on('click', function() {
       alert("Handle for logout called");
       $.ajax({
         url: `/logout`,
         method: 'POST',
-        success: function(result){
-          console.log(result)
-          if(result) {
+        success: function(result) {
+          console.log(result);
+          if (result) {
             console.log(result);
             const loginButton = document.getElementById("login-button");
             const logoutButton = document.getElementById("logout-button");
@@ -480,17 +285,22 @@ renderMenu();
             logoutButton.style.display = "none";
           }
         }
-      })
+      });
     });
-  })
+  });
 
-})
+  navbar();
+  openUserLoginForm();
+  renderMenu();
 
+});
+
+//--------------------------------Used to render the modal form ----------------------------------------------
 
 function toggleModal() {
-  const body = document.querySelector('body')
-  const modal = document.querySelector('.modal')
-  modal.classList.toggle('opacity-0')
-  modal.classList.toggle('pointer-events-none')
-  body.classList.toggle('modal-active')
+  const body = document.querySelector('body');
+  const modal = document.querySelector('.modal');
+  modal.classList.toggle('opacity-0');
+  modal.classList.toggle('pointer-events-none');
+  body.classList.toggle('modal-active');
 }
